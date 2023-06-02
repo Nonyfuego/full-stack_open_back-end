1dd06d322a2a15656c2367e89f7702ee4c01a180
nonyfuego <nonykuti@gmail.com>
2023-06-02 14:44:26 +0100

    add 3.3: Phonebook backend step3

--- part-3/phonebook_backend/index.js
+++ part-3/phonebook_backend/index.js
@@ -38,6 +38,16 @@ app.get('/info', (_req, res) => {
     )
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    let id = Number(req.params.id)
+    let person = persons.find(p => p.id === id)
+    if (!person) {
+        res.status(404).end()
+    } else {
+        res.json(person)
+    }   
+})
+
 app.listen(PORT, () => {
     console.log('app listening in port', PORT)
 })
